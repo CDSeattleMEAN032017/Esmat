c0cd7e7a74499a2e595555583a2eba8ece1d34e8
Esmatullah Mohammadi <EsmatMohammadi@10003.local>
2017-03-08 14:49:57 -0800

    JS assignment

---
+++ JS_Function.js
@@ -0,0 +1,45 @@
+
+function Name() {
+    var Name = "Esmat";
+    console.log(Name +" "+ "is awesome");
+
+}
+    Name()
+
+var x = function(){
+    console.log("This is the output")
+}
+
+ x()
+
+function sayName(name, age){
+    console.log(name + " "+"is awesome and he is "+" " + age)
+}
+
+     sayName('Esmat', 19)
+
+
+function student(name, age, clas) {
+    if (age <= 6 ){
+        console.log(name + " you are not eligable to come to school ")
+
+    }else {
+        console.log(name + " Welcome to first class you are above six year old your age is "+ age)
+    }
+
+}
+    student("Esmat", 3, "first")
+
+    console.log("===============================")
+
+    function CreateCat(name, age) {
+        var cat={
+            name:name,
+            age:age
+        }
+        return cat
+    }
+    var myCat1 = CreateCat('fred', 10);
+    var myCat2 = CreateCat('bill', 17);
+    var myCat3 = CreateCat('someone', 11)
+    console.log(myCat2, myCat1, myCat3)
